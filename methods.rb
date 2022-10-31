bacd78596d52e2b000cdc3881df8899a6cd1256c
Soren <sorenisme@gmail.com>
2022-10-31 11:51:55 -0400

    commit tree d11b14f971ad68189c07f0157a29edf2843ab703

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+def my_function(num)
+    puts "Running myFunction"
+    num + 1
+end    
+
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve number
+    if (typeof number !== "number") return nill;
+    number / 2
+end
